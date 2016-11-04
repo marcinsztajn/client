eb081c32fe1f2bf24517e34dbc90b4322b67874e
Arkady667 <arkadiusz.czuba@gmail.com>
2016-11-04 11:01:24 +0100

    Dodanie argumentów początkowych, Wysyłanie operacji i parametrów

--- udp.c
+++ udp.c
@@ -5,9 +5,18 @@
 #include <netinet/in.h>
 #include <string.h>
 
-int main(){
-  int clientSocket, portNum, nBytes;
-  char buffer[1024];
+char dod[]="dodawanie";
+char mno[]="mnozenie";
+char odej[]="odejmowanie";
+char dzie[]="dzielenie";
+float operacja;
+
+
+int main( int argc, char * argv[]){
+  int clientSocket, portNum;
+  float nBytes;
+  float buffer[3];
+   
   struct sockaddr_in serverAddr;
   socklen_t addr_size;
 
@@ -23,20 +32,45 @@ int main(){
   /*Initialize size variable to be used later on*/
   addr_size = sizeof serverAddr;
 
-  while(1){
-    printf("Type a sentence to send to server:\n");
-    fgets(buffer,1024,stdin);
-    printf("You typed: %s",buffer);
+  if(strcmp(dod,argv[3])==0){
+	operacja=1.0;
+  }
+  if(strcmp(odej,argv[3])==0){
+	operacja=2.0;
+  }
+  if(strcmp(mno,argv[3])==0){
+	operacja=3.0;
+  }
+  if(strcmp(dzie,argv[3])==0){
+	operacja=4.0;
+  }	
+	
+	
 
-    nBytes = strlen(buffer) + 1;
+
+  while(1){
+    //printf("Type a sentence to send to server:\n");
+    //fgets(buffer,1024,stdin);
+    //printf("You typed: %s",buffer);
+	//strncpy(buffer,atoi(argv[1]),sizeof(argv[1]));
+	buffer[0]=atof(argv[1]);
+	buffer[1]=atof(argv[2]);
+	buffer[2]=atof(operacja);
+	
+    nBytes = sizeof(buffer)+1 ;
     
     /*Send message to server*/
     sendto(clientSocket,buffer,nBytes,0,(struct sockaddr *)&serverAddr,addr_size);
 
     /*Receive message from server*/
-                nBytes = recvfrom(clientSocket,buffer,1024,0,NULL, NULL);
+    nBytes = recvfrom(clientSocket,buffer,sizeof(buffer),0,NULL, NULL);
 
-    printf("Received from server: %s\n",buffer);
+    ///*Send message to server*/
+    //sendto(clientSocket,buffer,nBytes,0,(struct sockaddr *)&serverAddr,addr_size);
+
+	
+
+    printf("Wynik operacji arytmetycznej: %f\n",buffer);
 
   }
 
